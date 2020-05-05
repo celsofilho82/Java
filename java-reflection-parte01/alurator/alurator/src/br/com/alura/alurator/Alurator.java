8d520e965ec7c9d1640ade3c5634ac1dc6d801ed
Celso Ribeiro <celsofilho82@gmail.com>
2020-05-04 23:10:09 -0300

    Starting with Reflection

--- java-reflection-parte01/alurator/alurator/src/br/com/alura/alurator/Alurator.java
+++ java-reflection-parte01/alurator/alurator/src/br/com/alura/alurator/Alurator.java
@@ -1,11 +1,37 @@
 package br.com.alura.alurator;
 
+import javax.management.RuntimeErrorException;
+
 public class Alurator {
-	
+
+	private String pacoteBase;
+
+	public Alurator(String pacoteBase) {
+		this.pacoteBase = pacoteBase;
+	}
+
 	public Object executa(String url) {
 		// TODO - processa a requisicao executando o metodo
 		// da classe em questao
-		
-		return null;
+
+		// produto/lista
+		String[] partesUrl = url.replaceFirst("/", "").split("/");
+
+		String nomeControle = Character.toUpperCase(partesUrl[0].charAt(0)) + partesUrl[0].substring(1) + "Controller";
+
+		try {
+			Class<?> classControle = Class.forName(pacoteBase + nomeControle);
+
+			Object instanciaControle = classControle.newInstance();
+
+			System.out.println(instanciaControle);
+
+			return null;
+
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 }
