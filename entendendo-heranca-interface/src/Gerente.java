d81c64c3e89800cebbfe3d5a3b408ae275dc31fe
Celso Ribeiro <celsofilho82@gmail.com>
2020-05-18 21:59:50 -0300

    Super e reescrita de métodos

--- entendendo-heranca-interface/src/Gerente.java
+++ entendendo-heranca-interface/src/Gerente.java
@@ -12,7 +12,7 @@ public class Gerente extends Funcionario {
 	}
 	
 	public double getBonificacao() {
-		return this.getSalario();
+		return super.getBonificacao() + super.getSalario();
 	}
 
 	public int getSenha() {
