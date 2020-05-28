1185402c1dd3b7377b5679d8a9f08efc2ca56284
Celso Ribeiro <celsofilho82@gmail.com>
2020-05-27 21:39:10 -0300

    Tratamento de exceções

--- entendendo-excecoes/src/Fluxo.java
+++ entendendo-excecoes/src/Fluxo.java
@@ -2,22 +2,33 @@
 public class Fluxo {
 
 	public static void main(String[] args) {
-        System.out.println("Ini do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
+		System.out.println("Ini do main");
+		metodo1();
+		System.out.println("Fim do main");
+	}
 
-    private static void metodo1() {
-        System.out.println("Ini do metodo1");
-        metodo2();
-        System.out.println("Fim do metodo1");
-    }
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
 
-    private static void metodo2() {
-        System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-        System.out.println("Fim do metodo2");
-    }
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			// evitando que a exceção caia na pilha
+			try {
+				// criando uma exceção ArithmeticException!
+				int a = i / 0;
+			} catch (ArithmeticException e) {
+				// capturando a exceção, mensagem e pilha
+				String msg = e.getMessage();
+				System.out.println("ArithmeticException" + msg);
+				e.printStackTrace();
+			}
+
+		}
+		System.out.println("Fim do metodo2");
+	}
 }
