92ae66fdc10afe22ede7b7cb16e8830f59548a73
Celso Ribeiro <celsofilho82@gmail.com>
2020-06-12 18:42:42 -0300

    Escopo de dependências

--- maven/lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
+++ maven/lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.caelum.stella.tinytype.CPF;
+
 @WebServlet(urlPatterns = {"/contato"})
 public class ContatoServlet extends HttpServlet {
 
