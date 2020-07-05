9c2f3cf8563b02b61c97dd3821ba88b68703a913
Celso Ribeiro <celsofilho82@gmail.com>
2020-07-04 22:16:02 -0300

    Realizando a validação dos dados

--- Spring-MVC-parte01/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ Spring-MVC-parte01/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,8 +2,14 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.BindingResultUtils;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,10 +18,20 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.DAO.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos") // Informando ao Spring que a classe vai responder na rota
 public class ProdutosController {
+
+	// Método utilizado para fazer o binding com a classe de validação do nosso
+	// produto usando a anotação @InitBinder
+	// Esse método recebe um objeto WebDataBinder e dentro desse objeto passamos o
+	// nosso validador
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new ProdutoValidation());
+	}
 
 	@Autowired // Pedimos para o Spring uma instância do objeto que foi anotado
 	private ProdutoDAO produtoDao;
@@ -33,12 +49,20 @@ public class ProdutosController {
 
 	// Metódo responsável por receber os dados da pagina produtos/form, inserir o
 	// produto no banco e redireciona o usuário para a página de listar produtos
+	// Usando a anotação @Valid para Spring validar o produto
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes) {
-		System.out.println(produto);
+	public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes) {
+
+		// Verificando se hover erro de validação, o usuário será redirecionado para o
+		// form
+		if (result.hasErrors()) {
+			return form();
+		}
+
 		produtoDao.gravar(produto);
 		ModelAndView modelAndView = new ModelAndView("redirect:produtos");
-		// Flash Scoped adicionamos um objeto que devemos enviar de um request para o outro
+		// Flash Scoped adicionamos um objeto que devemos enviar de um request para o
+		// outro
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!!");
 		return modelAndView;
 	}
---
+++ Spring-MVC-parte01/casadocodigo/src/main/java/br/com/casadocodigo/loja/validation/ProdutoValidation.java
@@ -0,0 +1,31 @@
+package br.com.casadocodigo.loja.validation;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+
+import br.com.casadocodigo.loja.models.Produto;
+
+// Essa classe tem como objetivo realizar a validação dos dados inseridos no formulário
+public class ProdutoValidation implements org.springframework.validation.Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// Esse método indica que podemos chamar o validador para a nossa classe produto
+		return Produto.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// Realizando a validação do formulário usando a classe do Spring
+		// ValidationUtils
+		Produto produto = (Produto) target;
+		ValidationUtils.rejectIfEmpty(errors, "titulo", "field.required");
+		ValidationUtils.rejectIfEmpty(errors, "descricao", "field.required");
+
+		if (produto.getPaginas() <= 0) {
+			errors.rejectValue("paginas", "field.required");
+		}
+
+	}
+
+}
