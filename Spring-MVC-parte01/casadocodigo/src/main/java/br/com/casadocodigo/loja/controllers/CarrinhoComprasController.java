0b8472e0d432e126538468dd2216fdb0e3ccbf6a
Celso Ribeiro <celsofilho82@gmail.com>
2020-07-07 22:17:10 -0300

    Inserindo os itens no carrinho de compras

--- Spring-MVC-parte01/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
+++ Spring-MVC-parte01/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/CarrinhoComprasController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,7 +30,7 @@ public class CarrinhoComprasController {
 	@RequestMapping("/add")
 	public ModelAndView add(Integer produtoId, TipoPreco tipoPreco) {
 
-		ModelAndView modelAndView = new ModelAndView("redirect:/produtos");
+		ModelAndView modelAndView = new ModelAndView("redirect:/carrinho");
 		CarrinhoItem carrinhoItem = CriaItem(produtoId, tipoPreco);
 
 		carrinho.add(carrinhoItem);
@@ -36,10 +38,17 @@ public class CarrinhoComprasController {
 		return modelAndView;
 	}
 
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView itens() {
+		ModelAndView modelAndView = new ModelAndView("/carrinho/itens");
+		
+		return modelAndView;
+	}
+	
 	private CarrinhoItem CriaItem(Integer produtoId, TipoPreco tipoPreco) {
 		Produto produto = produtoDAO.find(produtoId);
 		CarrinhoItem carrinhoItem = new CarrinhoItem(produto, tipoPreco);
 		return carrinhoItem;
 	}
-
+	
 }
