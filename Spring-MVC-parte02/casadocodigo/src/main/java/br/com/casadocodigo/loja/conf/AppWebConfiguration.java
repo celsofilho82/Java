155fe00443213f6eca651f30b6126f3439a1d10c
Celso Ribeiro <celsofilho82@gmail.com>
2020-07-20 21:17:47 -0300

    Implementando um serviço Rest

--- Spring-MVC-parte02/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ Spring-MVC-parte02/casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -13,12 +15,15 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.google.common.cache.CacheBuilder;
@@ -30,67 +35,81 @@ import br.com.casadocodigo.loja.models.CarrinhoCompras;
 
 @EnableCaching
 @EnableWebMvc
-@ComponentScan(basePackageClasses= {HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class})
+@ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class, FileSaver.class, CarrinhoCompras.class })
 public class AppWebConfiguration extends WebMvcConfigurerAdapter {
-	
+
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix( ".jsp");
-		
-		//resolver.setExposeContextBeansAsAttributes(true);
+		resolver.setSuffix(".jsp");
+
+		// resolver.setExposeContextBeansAsAttributes(true);
 		resolver.setExposedContextBeanNames("carrinhoCompras");
-		
+
 		return resolver;
 	}
 
 	@Bean
-	public MessageSource messageSource(){
+	public MessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
 		messageSource.setBasename("/WEB-INF/messages");
 		messageSource.setDefaultEncoding("UTF-8");
 		messageSource.setCacheSeconds(1);
 		return messageSource;
 	}
-	
+
 	@Bean
 	public FormattingConversionService mvcConversionService() {
-		DefaultFormattingConversionService conversionService = 
-				new DefaultFormattingConversionService();
+		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
 		DateFormatterRegistrar registrar = new DateFormatterRegistrar();
 		registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
 		registrar.registerFormatters(conversionService);
-		
+
 		return conversionService;
 	}
-	
-    @Bean
-	public MultipartResolver multipartResolver(){
+
+	@Bean
+	public MultipartResolver multipartResolver() {
 		return new StandardServletMultipartResolver();
 	}
-    
+
 	@Bean
-	public RestTemplate restTemplate(){
+	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-    
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
-    
-    // Configurando qual mecanismo de cache deverá ser utilizado
-    // neste caso o Guava
-    @Bean
-    public CacheManager cacheManager() {
-        CacheBuilder<Object, Object> builder = 
-            CacheBuilder.newBuilder().maximumSize(100)
-                .expireAfterAccess(5, TimeUnit.MINUTES);
-        GuavaCacheManager manager = new GuavaCacheManager();
-        manager.setCacheBuilder(builder);
 
-        return manager;
-    }
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	// Configurando qual mecanismo de cache deverá ser utilizado
+	// neste caso o Guava
+	@Bean
+	public CacheManager cacheManager() {
+		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder().maximumSize(100).expireAfterAccess(5,
+				TimeUnit.MINUTES);
+		GuavaCacheManager manager = new GuavaCacheManager();
+		manager.setCacheBuilder(builder);
+
+		return manager;
+	}
+
+	// Métodos responsável por agrupar os resolvedores de views, os que retornam
+	// JSP(internalResourceViewResolver()) e o que retronar uma
+	// JSON(JsonViewResolver())
+	@Bean
+	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager) {
+
+		List<ViewResolver> viewResolvers = new ArrayList<>();
+		viewResolvers.add(internalResourceViewResolver());
+		viewResolvers.add(new JsonViewResolver());
+
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(viewResolvers);
+		resolver.setContentNegotiationManager(manager);
+
+		return resolver;
+	}
 }
-	
